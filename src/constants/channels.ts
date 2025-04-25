eeef170062b3ee806e96b4c89edee1308451ec5b
Dizo <jbr1989@gmail.com>
2025-04-25 19:06:25 +0200

    reduce images

--- src/constants/channels.ts
+++ src/constants/channels.ts
@@ -3,18 +3,18 @@ export const channelsConst = [
 		title: "Elisa Waves",
 		tag: "@elisawavesroleplay",
 		id: "UCmjo9V6ptM_Cdx67X5UXJEw",
-		logo: "/img/channels/elisawaves.jpg",
+		logo: "/img/channels/elisawaves.webp",
 	},
 	{
 		title: "Elisa Motion",
 		tag: "@elisamotion",
 		id: "UCsLebFG6jRTC0KUkN_bLcjw",
-		logo: "/img/channels/elisamotion.jpg",
+		logo: "/img/channels/elisamotion.webp",
 	},
 	{
 		title: "Elisa Waves TV",
 		tag: "@elisawavestv",
 		id: "UCskFPl21j0IsryvBsq-EbDw",
-		logo: "/img/channels/elisawavestv.jpg",
+		logo: "/img/channels/elisawavestv.webp",
 	},
 ];
