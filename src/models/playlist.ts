aa9bab139c11d0e757020c5470b3bddd0cc2fcbc
Dizo <jbr1989@gmail.com>
2025-04-29 23:17:20 +0200

    SEO update

--- src/models/playlist.ts
+++ src/models/playlist.ts
@@ -3,9 +3,15 @@ export class Playlist {
 	title: string;
 	image: string;
 
+	channelId: string;
+	channelTitle: string;
+
 	constructor(playlistYoutube) {
 		this.id = playlistYoutube.id;
 		this.title = playlistYoutube.snippet.title;
 		this.image = playlistYoutube.snippet.thumbnails.medium.url;
+
+		this.channelId = playlistYoutube.snippet.channelId;
+		this.channelTitle = playlistYoutube.snippet.channelTitle;
 	}
 }
