55f31dd72b6845d962e333f9d9a2436a4181269b
Dizo <jbr1989@gmail.com>
2025-04-29 23:32:44 +0200

    canonical tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://elisawaves.es",
 	vite: {
 		plugins: [tailwindcss()],
 	},
