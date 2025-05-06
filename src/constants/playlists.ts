2698c64b2828bea211afca52a816cd0928215eb0
Dizo <jbr1989@gmail.com>
2025-05-06 19:32:55 +0200

    CHATS: M. Mackenzie

--- src/constants/playlists.ts
+++ src/constants/playlists.ts
@@ -1,3 +1,18 @@
 export const playlistsConst = {
+	// Lost Records
 	PT7kwCk4N80: 1534, //00:25:36
+
+	// ROL: M. MacKenzie
+	i1oaYkTYGNA: 0, //  🧡 M. MacKenzie 🧡 || T01x01: "Long Time Ago" || ✨≋ELISAWAVES≋ ✨
+	QbQoICZb6mE: 0, //  🧡 M. Mackenzie 🧡 || T01x02: "Marcaichean is Gàrraidhean" || ✨≋ELISAWAVES≋ ✨
+	// 3 ??
+	pPOxEdCeywM: 0, //  🧡 M. Mackenzie 🧡 || T01x04: "Leugh Le Gràdh" || ✨≋ELISAWAVES≋ ✨
+	nbeYW843U5w: 0, //  🧡 M. Mackenzie 🧡 || T01x05: "fosgladh" || ✨≋ELISAWAVES≋ ✨
+	UXVBMEPNLCE: 0, //  🧡 M. MacKenzie 🧡 || T01x06: "Campa ùr" || ✨≋ELISAWAVES≋ ✨
+	wPXLQ0sgd9Y: 0, //  🧡 M. MacKenzie 🧡 || T01x07: "A' feuchainn ri mairsinn (mar is lugha)" || ✨≋ELISAWAVES≋ ✨
+	"5X6YBndn2YU": 0, //  🧡 M. MacKenzie 🧡 || T01x08: "coinneamh" || ✨≋ELISAWAVES≋✨
+	WzvG_vzmm08: 0, //  🧡 M. MacKenzie 🧡 || T01x09: "Flùr a tha a 'fàs" ||✨≋ELISAWAVES≋✨
+	_L7bsdtOvPQ: 0, //  🧡 M. Mackenzie🧡 || T01x10: "A' choinneamh" || ✨≋ELISAWAVES≋ ✨
+	NEDQcvBvU1g: 0, //  🧡 M. Mackenzie 🧡 || T01x11: "An Toitean" || ✨≋ELISAWAVES≋✨
+	I29f4_48CBw: 0, //  🧡 M. Mackenzie 🧡 || T01x12: "Cogadh" || ✨≋ELISAWAVES≋✨
 };
